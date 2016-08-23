fb1b2719fb801c0e861dfe6d682e6597bfc23395
Salahuddin Munchi <salah_cse_mbstu@yahoo.com>
2016-08-23 16:39:33 +0600

    file upload related work and integrated with jquery file upload and need to be manipulated with cropper in jquery

--- js/script.js
+++ js/script.js
@@ -17,3 +17,23 @@ function removeKeyword(obj) {
     obj.remove();
     $('#eventForm').submit();
 }
+
+function doUpload(obj, url) {
+    var containerObj = $('.photo_preloader');
+    obj.fileupload({
+        url: url,
+        dataType: 'json',
+        add: function(e, data) {
+            containerObj.html('<img src="../../images/preloader.gif">');
+            data.submit();
+        },
+        done: function (e, data) {
+            containerObj.html('');
+            $.each(data.result.files, function (index, file) {               
+                containerObj.html('<img src="' + file.url + '" width="50"/>');
+            });
+            $('input:submit').attr('disabled', false);
+        }
+    }).prop('disabled', !$.support.fileInput)
+    .parent().addClass($.support.fileInput ? undefined : 'disabled');
+}
--- protected/controllers/EventController.php
+++ protected/controllers/EventController.php
@@ -11,29 +11,29 @@ class EventController extends Controller {
     public function actionEventList() {
         $dataProvider = new CActiveDataProvider('SystemEvent');
         $popularKeyword = Yii::app()->params['eventKeyword']['School Based'];
-        
+
         $this->render('eventList', array('dataProvider' => $dataProvider, 'popularKeyword' => $popularKeyword));
     }
 
     public function actionEventFilter() {
         $criteria = new CDbCriteria();
-        $criteria->select='*';
-        $criteria->alias='E';
+        $criteria->select = '*';
+        $criteria->alias = 'E';
 
         if ($q = $_GET['q']) {
             $criteria->compare('title', $q, true, 'OR');
             $criteria->compare('description', $q, true, 'OR');
         }
-        
-        if($keyword = $_GET['keyword']) {
-            foreach($keyword as $key) {
-                $content[] = ' keyword.name="'.$key.'" ';
+
+        if ($keyword = $_GET['keyword']) {
+            foreach ($keyword as $key) {
+                $content[] = ' keyword.name="' . $key . '" ';
             }
-            $condition = implode('AND', $content);            
+            $condition = implode('AND', $content);
             $criteria->join = 'inner join keyword on keyword.eventId = E.id';
             $criteria->condition = $condition;
         }
-       
+
         $dataProvider = new CActiveDataProvider('SystemEvent', array('criteria' => $criteria));
         $popularKeyword = Yii::app()->params['eventKeyword']['School Based'];
         $this->render('eventList', array('dataProvider' => $dataProvider, 'popularKeyword' => $popularKeyword));
@@ -43,52 +43,57 @@ class EventController extends Controller {
         if (isset($_POST['create'])) {
             $event = new Event($_POST);
             $event->save();
+            $obj = new UploadHandler();
             $this->redirect(array('/event/eventList'));
         }
-        
-        if(isset($_POST['preview'])) {
+
+        if (isset($_POST['preview'])) {
             Yii::app()->session['eventData'] = $_POST;
             $this->redirect(array('/event/addKeyword'));
-        }        
-        
+        }
+
         $this->render('create');
     }
-    
+
     public function actionAddKeyword() {
-       $eventKeyword = Yii::app()->params['eventKeyword'];
-       $eventData = Yii::app()->session['eventData'];      
-       $existingKeyword = !empty($eventData['keyword']) ? $eventData['keyword']: [];
-       
-       if(isset($_REQUEST['keyword'])) {
-           foreach($_REQUEST['keyword'] as $keyword) {
-               if(!in_array($keyword, $existingKeyword)) {
-                   array_push($existingKeyword, $keyword);
-               }
-           }
-       }
-       
-       $eventData['keyword'] = $existingKeyword;
-       Yii::app()->session['eventData'] =  $eventData;
-       $this->render('addKeyword', array('eventKeyword'=>$eventKeyword, 'existingKeyword'=>$eventData['keyword'])); 
+        $eventKeyword = Yii::app()->params['eventKeyword'];
+        $eventData = Yii::app()->session['eventData'];
+        $existingKeyword = !empty($eventData['keyword']) ? $eventData['keyword'] : [];
+
+        if (isset($_REQUEST['keyword'])) {
+            foreach ($_REQUEST['keyword'] as $keyword) {
+                if (!in_array($keyword, $existingKeyword)) {
+                    array_push($existingKeyword, $keyword);
+                }
+            }
+        }
+
+        $eventData['keyword'] = $existingKeyword;
+        Yii::app()->session['eventData'] = $eventData;
+        $this->render('addKeyword', array('eventKeyword' => $eventKeyword, 'existingKeyword' => $eventData['keyword']));
     }
-    
+
     public function actionPreview() {
         $eventData = Yii::app()->session['eventData'];
         $this->render('view', array('eventData' => $eventData));
     }
-    
+
     public function actionView() {
         $eventId = Yii::app()->request->getParam('id');
         $event = new Event($eventId);
         $eventData = $event->findById();
         $this->render('view', array('eventData' => $eventData));
     }
-    
+
     public function actionSave() {
         $eventData = Yii::app()->session['eventData'];
         $event = new Event($eventData);
         $event->save();
-        Yii::app()->session->remove('eventData');        
+        Yii::app()->session->remove('eventData');
         $this->redirect(array('/event/eventList'));
+    }
+
+    public function actionUpload() {        
+        $obj = new UploadHandler();
     }
 }
